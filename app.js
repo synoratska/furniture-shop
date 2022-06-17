4ae78a32ee7184e1f1f331e408b85ba27bfbd932
synoratska <m.synoratska@gmail.com>
2022-06-17 16:50:02 +0300

    getting products from json file

---
+++ app.js
@@ -0,0 +1,48 @@
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+// Main cart
+let cart = [];
+
+// getting the products
+class Products {
+  async getProducts() {
+    try {
+      let result = await fetch("products.json");
+      let data = await result.json();
+      let products = data.items;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+// display Products
+
+class UI {}
+
+// lacal storage
+
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const ui = new UI();
+  const products = new Products();
+
+  // get all products
+  products.getProducts().then((products) => console.log(products));
+});
